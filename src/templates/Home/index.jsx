498a1af315d4af3bb967c962d7e82001b20022f3
Cíntia <cintia.oliveeira@hotmail.com>
2021-07-30 20:25:38 -0300

    migrando load posts para hooks

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,92 +1,71 @@
-import { Component } from "react";
-import "./styles.css";
+import { useCallback, useEffect, useState } from "react";
 
 import { Posts } from "../../components/Posts";
-
 import { loadPosts } from "../../another/load-posts";
 import { Button } from "../../components/Button";
 import { TextInput } from "../../components/TextInput";
 
-export class Home extends Component {
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerPage: 4,
-    searchValue: ''
-  };
+import "./styles.css";
 
-  async componentDidMount() {
-    await this.loadPosts();
-  }
+export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(20);
+  const [searchValue, setSearchValue] = useState("");
 
-  loadPosts = async () => {
-    const { page, postsPerPage } = this.state;
-
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
     const postsAndPhotos = await loadPosts();
-    this.setState({
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos,
-    });
-  };
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, []);
 
-  loadMorePosts = () => {
-    const { page, postsPerPage, allPosts, posts } = this.state;
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+
+  const loadMorePosts = () => {
     const nextPage = page + postsPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
     posts.push(...nextPosts);
 
-    this.setState({ posts, page: nextPage });
-
-    console.log(page, postsPerPage, nextPage, nextPage + postsPerPage);
+    setPosts(posts);
+    setPage(nextPage);
   };
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { value } = e.target;
-    this.setState({ searchValue: value})
-  }
+    setSearchValue(value);
+  };
 
-  render() {
-    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length;
-
-    const filteredPosts = !!searchValue ? 
-    allPosts.filter(post => {
-      return post.title.toLowerCase().includes(searchValue.toLowerCase()
-      );
-    }) 
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+  const filteredPosts = !!searchValue
+    ? allPosts.filter((post) => {
+        return post.title.toLowerCase().includes(searchValue.toLowerCase());
+      })
     : posts;
 
-    return (
-      <section className="container">
-        <div className="search-container">
-          {!!searchValue && (
-            <h1>Search value: {searchValue}</h1>
-          )} 
+  return (
+    <section className="container">
+      <div className="search-container">
+        {!!searchValue && <h1>Search value: {searchValue}</h1>}
 
-          <TextInput searchValue={searchValue} handleChange={this.handleChange} />
-        </div>
-        
+        <TextInput searchValue={searchValue} handleChange={handleChange} />
+      </div>
 
-        {filteredPosts.length > 0 && (
-          <Posts posts={filteredPosts} />
+      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+
+      {filteredPosts.length === 0 && <p>Não existem posts =(</p>}
+
+      <div className="button-container">
+        {!searchValue && (
+          <Button
+            text="Load more posts"
+            onClick={loadMorePosts}
+            disabled={noMorePosts}
+          />
         )}
-        
-        {filteredPosts.length === 0 && (
-          <p>Não existem posts</p>
-        )}
-
-        <div className="button-container">
-          {!searchValue && ( // se não tiver busca, exiba o botão
-            <Button
-              text="Load more posts"
-              onClick={this.loadMorePosts} // neste caso, onClick não é evento é um atributo
-              disabled={noMorePosts}
-            />
-          )} 
-          
-        </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
